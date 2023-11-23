141f4fe0417a6a956680ec79825259edfbe023e1
Pragati Aliani <pragatialiani2003@gmail.com>
2023-11-23 11:43:31 +0530

    homepage added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connection");
 const urlRoute = require("./routes/url");
+const staticRoutes = require("./routes/staticRoutes");
 
 const app = express();
 const PORT = 8001;
@@ -9,8 +11,14 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
     console.log("MONGO DB CONNECTED")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+
+app.use("/", staticRoutes);
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
